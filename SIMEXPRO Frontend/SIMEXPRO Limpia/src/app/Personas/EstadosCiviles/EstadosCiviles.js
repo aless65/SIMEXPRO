9375fe5313864ae90bc06e8b64d401feceb847fa
Alex <saraieqp30@gmail.com>
2023-07-14 16:26:01 -0600

    modificaciones

--- SIMEXPRO Frontend/SIMEXPRO Limpia/src/app/Personas/EstadosCiviles/EstadosCiviles.js
+++ SIMEXPRO Frontend/SIMEXPRO Limpia/src/app/Personas/EstadosCiviles/EstadosCiviles.js
@@ -2,16 +2,18 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, Icon } from '@mui/material';
+import { Button, Icon, IconButton, InputAdornment, TextField } from '@mui/material';
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
 import { purple } from '@mui/material/colors';
 import { DataGrid } from '@mui/x-data-grid';
+import { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 
 const columns = [
-  { field: 'id', headerName: 'Código', width: 150 },
-  { field: 'descripcion', headerName: 'Descripción', width: 400 },
+  { field: 'id', headerName: 'Código', width: 200 },
+  { field: 'descripcion', headerName: 'Descripción', width: 300 },
   {
     field: 'acciones',
     headerName: 'Acciones',
@@ -21,7 +23,7 @@ const columns = [
         <Button
           startIcon={<Icon>edit</Icon>}
           variant="contained"
-          sx={{ backgroundColor: '#FFBD59', color: 'black' }}
+          sx={{ backgroundColor: '#634A9E', color: 'white', borderRadius: '10px', }}
         >
           Editar
         </Button>
@@ -29,7 +31,7 @@ const columns = [
           startIcon={<Icon>visibility</Icon>}
           variant="contained"
           color="primary"
-          sx={{ backgroundColor: '#DAD8D8', color: 'black' }}
+          sx={{ backgroundColor: '#797979', color: 'white',  borderRadius: '10px' }}
         >
           Detalles
         </Button>
@@ -37,7 +39,7 @@ const columns = [
           startIcon={<Icon>delete</Icon>}
           variant="contained"
           color="primary"
-          sx={{ backgroundColor: '#F44336', color: 'black' }}
+          sx={{ backgroundColor: '#E40F00', color: 'white',  borderRadius: '10px' }}
         >
           Eliminar
         </Button>
@@ -45,18 +47,18 @@ const columns = [
     ),
   },
 ];
-const ColorButton = styled(Button)(({ theme }) => ({
-    color: theme.palette.getContrastText(purple[500]),
-    borderRadius: '10px',
-    backgroundColor: '#634A9E',
-    '&:hover': {
-      backgroundColor: purple[700],
-    },
-    '& .MuiButton-startIcon': {
-      marginRight: theme.spacing(0.5),
-    },
-  }));
 
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  borderRadius: '10px',
+  backgroundColor: '#634A9E',
+  '&:hover': {
+    backgroundColor: purple[700],
+  },
+  '& .MuiButton-startIcon': {
+    marginRight: theme.spacing(0.5),
+  },
+}));
 
 const rows = [
   { id: 'C', descripcion: 'Casado' },
@@ -64,25 +66,70 @@ const rows = [
   { id: 'V', descripcion: 'Viudo' },
   { id: 'A', descripcion: 'Amante' },
   { id: 'D', descripcion: 'Divorciado' },
+  { id: 'E', descripcion: 'Separado' },
 ];
 
 function UsuariosIndex() {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredRows = rows.filter((row) =>
+    row.descripcion.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <Card sx={{ minWidth: 275, margin: '40px' }}>
       <CardMedia
         component="img"
         height="200"
-        image="https://i.ibb.co/T4VqYmN/Headers-SIMEXPRO-3.png"
+        image="https://i.ibb.co/DWv6wP7/ESTADOS-CIVILES-1.png"
         alt="Encabezado de la carta"
       />
-      <CardContent>
-      <ColorButton startIcon={<Icon>add</Icon>}>
-            Nuevo
-          </ColorButton>
-        <div style={{ height: 400, width: '100%' }}>
-          <DataGrid rows={rows} columns={columns} pageSize={5} />
-        </div>
+      <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+      <Stack direction="row" spacing={1}>
+      <Button
+          startIcon={<Icon>add</Icon>}
+          variant="contained"
+          color="primary"
+          sx={{ backgroundColor: '#634A9E', color: 'white',  borderRadius: '10px' }}
+        >
+          Nuevo
+        </Button>
+        </Stack>
+
+        <TextField
+       placeholder='Buscar'
+       borderRadius= '100px'
+          value={searchText}
+          onChange={handleSearchChange}
+          size="small"
+          variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton edge="start">
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
       </CardContent>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={filteredRows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+        />
+      </div>
     </Card>
   );
 }

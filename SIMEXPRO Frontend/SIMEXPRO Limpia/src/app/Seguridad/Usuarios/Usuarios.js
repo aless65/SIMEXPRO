08be70591a60c498344990b23aea0fe463b3a046
IsHatake <isaacramos191969@gmail.com>
2023-07-16 19:18:13 -0600

    Funcionalidad de Usuarios

--- SIMEXPRO Frontend/SIMEXPRO Limpia/src/app/Seguridad/Usuarios/Usuarios.js
+++ SIMEXPRO Frontend/SIMEXPRO Limpia/src/app/Seguridad/Usuarios/Usuarios.js
@@ -1,26 +1,120 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, Icon } from '@mui/material';
+import { Button, FormControl, Icon, IconButton, InputAdornment, InputLabel, TextField } from '@mui/material';
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
-import { purple } from '@mui/material/colors';
+import { DataGrid, GridToolbar, esES } from '@mui/x-data-grid'
+import { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 
-const ColorButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText(purple[500]),
-  borderRadius: '10px',
-  backgroundColor: '#634A9E',
-  '&:hover': {
-    backgroundColor: purple[700],
-  },
-  '& .MuiButton-startIcon': {
-    marginRight: theme.spacing(0.5),
-  },
-}));
+import Zoom from '@mui/material/Zoom';
+import Grow from '@mui/material/Grow';
+
+import Collapse from '@mui/material/Collapse';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Select from '@mui/material/Select';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 function UsuariosIndex() {
+  const [searchText, setSearchText] = useState('');
+  const [mostrarIndex, setmostrarIndex] = useState(true);
+  const [mostrarAdd, setmostrarAdd] = useState(false);
+  const [Eliminar, setEliminar] = useState(false);
+
+  const DialogEliminar = () => {
+    setEliminar(!Eliminar);
+  };
+
+  {/*Columnas de la tabla*/ }
+  const columns = [
+    { field: 'id', headerName: 'Id', width: 10 },
+    { field: 'usuario', headerName: 'Usuario', flex: 1 },
+    { field: 'empleado', headerName: 'Empleado', flex: 1 },
+    { field: 'rol', headerName: 'Rol', flex: 1 },
+    {
+      field: 'acciones',
+      headerName: 'Acciones',
+      width: 400,
+      renderCell: (params) => (
+        <Stack direction="row" spacing={1}>
+          <Button
+            startIcon={<Icon>edit</Icon>}
+            variant="contained"
+            style={{ borderRadius: '10px' }}
+            sx={{
+              backgroundColor: '#634A9E',
+              color: 'white',
+              "&:hover": { backgroundColor: '#6e52ae' },
+            }}>
+            Editar
+          </Button>
+
+          <Button
+            startIcon={<Icon>visibility</Icon>}
+            variant="contained"
+            color="primary"
+            style={{ borderRadius: '10px' }}
+            sx={{
+              backgroundColor: '#797979', color: 'white',
+              "&:hover": { backgroundColor: '#b69999' },
+            }}
+          >
+            Detalles
+          </Button>
+          <Button
+            startIcon={<Icon>delete</Icon>}
+            variant="contained"
+            color="primary"
+            style={{ borderRadius: '10px' }}
+            sx={{
+              backgroundColor: '#E40F00', color: 'white',
+              "&:hover": { backgroundColor: '#eb5f56' },
+            }}
+            onClick={DialogEliminar}
+          >
+            Eliminar
+          </Button>
+        </Stack>
+      ),
+    },
+  ];
+
+
+  {/*Datos de la tabla*/ }
+  const rows = [
+    { id: '1', usuario: 'IsHatake', empleado: 'Jafet Gomez', rol: 'Administrador de Seguridad' },
+    { id: '2', usuario: 'Admin', empleado: 'Jafet Gomez', rol: 'Administrador de Seguridad' },
+    { id: '3', usuario: 'Shogun', empleado: 'Jafet Gomez', rol: 'Administrador de Seguridad' },
+
+  ];
+
+  {/*Función para mostrar la tabla y mostrar agregar*/ }
+  const VisibilidadTabla = () => {
+    setmostrarIndex(!mostrarIndex);
+    setmostrarAdd(!mostrarAdd);
+  };
+
+
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  {/*Filtrado de datos*/ }
+  const filteredRows = rows.filter((row) =>
+    row.usuario.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <Card sx={{ minWidth: 275, margin: '40px' }}>
       <CardMedia
@@ -29,15 +123,223 @@ function UsuariosIndex() {
         image="https://i.ibb.co/T4VqYmN/Headers-SIMEXPRO-3.png"
         alt="Encabezado de la carta"
       />
-      <CardContent>
-        <Stack direction="row" spacing={2} alignItems="center">
-          <ColorButton startIcon={<Icon>add</Icon>}>
-            Nuevo
-          </ColorButton>
-        </Stack>
-      </CardContent>
+      <Collapse in={mostrarIndex}>
+        <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+
+          {/*Botón de Nuevo*/}
+          <Stack direction="row" spacing={1}>
+            <Button
+              startIcon={<Icon>add</Icon>}
+              variant="contained"
+              color="primary"
+              style={{ borderRadius: '10px' }}
+              sx={{
+                backgroundColor: '#634A9E', color: 'white',
+                "&:hover": { backgroundColor: '#6e52ae' },
+              }}
+              onClick={VisibilidadTabla}
+            >
+              Nuevo
+            </Button>
+          </Stack>
+
+          {/*Barra de Busqueda en la Tabla*/}
+          <TextField
+            style={{ borderRadius: '10px' }}
+            placeholder='Buscar'
+            value={searchText}
+            onChange={handleSearchChange}
+            size="small"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton edge="start">
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </CardContent>
+      </Collapse>
+
+      {/*Tabla*/}
+      <Collapse in={mostrarIndex}>
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+            components={{
+              Toolbar: GridToolbar,
+              Search: SearchIcon,
+            }}
+            rows={filteredRows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 10 },
+              },
+            }}
+            pageSizeOptions={[10, 20, 50]}
+          />
+        </div>
+      </Collapse>
+
+
+      {/*Formulario Agregar*/}
+      <Collapse in={mostrarAdd}>
+        <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography variant="h5" gutterBottom>
+                Nuevo Usuario
+              </Typography>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl
+                fullWidth
+              >
+                <TextField
+                  style={{ borderRadius: '10px' }}
+                  label="Usuario"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl
+                fullWidth
+              >
+                <TextField
+                  style={{ borderRadius: '10px' }}
+                  label="Contraseña"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl
+                fullWidth
+              >
+                <InputLabel htmlFor="grouped-native-select">Empleado</InputLabel>
+                <Select
+                  style={{ borderRadius: '10px' }}
+                  label="Empleado"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl
+                fullWidth
+              >
+                <InputLabel htmlFor="grouped-native-select">Rol</InputLabel>
+                <Select
+                  style={{ borderRadius: '10px' }}
+                  label="Rol"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <FormControl fullWidth>
+                <FormControlLabel
+                  control={<Switch sx={{ '&.Mui-checked': { color: '#634A9E' } }} />}
+                  label="Administrador"
+                  labelPlacement="bottom"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'right', alignItems: 'right' }} >
+              <Button
+                startIcon={<Icon>checked</Icon>}
+                variant="contained"
+                color="primary"
+                style={{ borderRadius: '10px', marginRight: '10px' }}
+                sx={{
+                  backgroundColor: '#634A9E', color: 'white',
+                  "&:hover": { backgroundColor: '#6e52ae' },
+                }}
+                onClick={VisibilidadTabla}
+              >
+                Guardar
+              </Button>
+
+              <Button
+                startIcon={<Icon>close</Icon>}
+                variant="contained"
+                color="primary"
+                style={{ borderRadius: '10px' }}
+                sx={{
+                  backgroundColor: '#DAD8D8', color: 'black',
+                  "&:hover": { backgroundColor: '#BFBABA' },
+                }}
+                onClick={VisibilidadTabla}
+              >
+                Cancelar
+              </Button>
+            </Grid>
+
+          </Grid>
+        </CardContent>
+      </Collapse>
+
+
+      <Dialog
+        open={Eliminar}
+        fullWidth={'md'}
+        onClose={DialogEliminar}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Confirmación de Eliminación"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+          ¿Está seguro(a) que desea eliminar este registro?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'right', alignItems: 'right' }} >
+              <Button
+                startIcon={<Icon>checked</Icon>}
+                variant="contained"
+                color="primary"
+                style={{ borderRadius: '10px', marginRight: '10px' }}
+                sx={{
+                  backgroundColor: '#634A9E', color: 'white',
+                  "&:hover": { backgroundColor: '#6e52ae' },
+                }}
+                onClick={DialogEliminar}
+              >
+                Eliminar
+              </Button>
+
+              <Button
+                startIcon={<Icon>close</Icon>}
+                variant="contained"
+                color="primary"
+                style={{ borderRadius: '10px' }}
+                sx={{
+                  backgroundColor: '#DAD8D8', color: 'black',
+                  "&:hover": { backgroundColor: '#BFBABA' },
+                }}
+                onClick={DialogEliminar}
+              >
+                Cancelar
+              </Button>
+            </Grid>
+        </DialogActions>
+      </Dialog>
+
     </Card>
   );
 }
 
 export default UsuariosIndex;
+
+
+

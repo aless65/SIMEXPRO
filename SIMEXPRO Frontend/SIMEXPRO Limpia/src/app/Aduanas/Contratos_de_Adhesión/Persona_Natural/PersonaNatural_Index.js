c73106e46b42d4912f7e031cd230a83b446fbde1
itsInfinity03 <125475340+itsInfinity03@users.noreply.github.com>
2023-07-18 10:01:33 -0600

    Trabajito martes

--- SIMEXPRO Frontend/SIMEXPRO Limpia/src/app/Aduanas/Contratos_de_Adhesión/Persona_Natural/PersonaNatural_Index.js
+++ SIMEXPRO Frontend/SIMEXPRO Limpia/src/app/Aduanas/Contratos_de_Adhesión/Persona_Natural/PersonaNatural_Index.js
@@ -4,22 +4,15 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { Button, FormControl, Icon, IconButton, InputAdornment, InputLabel, TextField } from '@mui/material';
-import * as React from 'react';
+import { Button, Icon, IconButton, InputAdornment, TextField } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { DataGrid, GridToolbar, esES } from '@mui/x-data-grid'
 import { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 
-import Zoom from '@mui/material/Zoom';
-import Grow from '@mui/material/Grow';
 
 import Collapse from '@mui/material/Collapse';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Select from '@mui/material/Select';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';

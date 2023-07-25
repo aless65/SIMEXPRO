a16b98f9c28ae4ed667ce5aff91bfdaa67e7524b
itsInfinity03 <139721902+javintiktok@users.noreply.github.com>
2023-07-25 14:41:27 -0600

    repositorios aduanas

---
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.BusinessLogic/Services/ProduccionServices/ProduccionServices.cs
@@ -0,0 +1,17 @@
+﻿
+using SIMEXPRO.DataAccess.Repositories.Adua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMEXPRO.BussinessLogic.Services.GeneralServices
+{
+    public class ProduccionServices
+    {
+        private readonly AduanasRepository _aduanasRepository;
+
+
+    }
+}
---
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Adua/DucaHistorial.cs
@@ -0,0 +1,37 @@
+﻿using SIMEXPRO.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMEXPRO.DataAccess.Repositories.Adua
+{
+    public class DucaHistorial : IRepository<tbDucaHistorial>
+    {
+        public RequestStatus Delete(tbDucaHistorial item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public tbDucaHistorial Find(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestStatus Insert(tbDucaHistorial item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<tbDucaHistorial> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestStatus Update(tbDucaHistorial item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

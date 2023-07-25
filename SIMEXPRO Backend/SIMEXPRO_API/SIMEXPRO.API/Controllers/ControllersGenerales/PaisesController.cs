fe446ec99ea0e4d90e8aed61531e6ed214bd5aa2
itsInfinity03 <139721902+javintiktok@users.noreply.github.com>
2023-07-25 13:59:45 -0600

    otro

---
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Controllers/ControllersGenerales/PaisesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIMEXPRO.API.Controllers.ControllersGenerales
+{
+    public class PaisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

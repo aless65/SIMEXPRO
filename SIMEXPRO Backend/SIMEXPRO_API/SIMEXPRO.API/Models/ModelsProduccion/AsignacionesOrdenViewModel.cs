4d28382e610fe31f0f5ef64273636667a834979b
Alex <saraieqp30@gmail.com>
2023-07-26 13:11:42 -0600

    Avance en Modulo de produccion
    Creacion de controller y view models de Produccion

---
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Models/ModelsProduccion/AsignacionesOrdenViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIMEXPRO.API.Models.ModelsProduccion
+{
+    public class AsignacionesOrdenViewModel
+    {
+        public int asor_Id { get; set; }
+        public int asor_OrdenDetId { get; set; }
+        public DateTime asor_FechaInicio { get; set; }
+        public DateTime asor_FechaLimite { get; set; }
+        public int asor_Cantidad { get; set; }
+        public int proc_Id { get; set; }
+        public int empl_Id { get; set; }
+        public int usua_UsuarioCreacion { get; set; }
+        public DateTime asor_FechaCreacion { get; set; }
+        public int? usua_UsuarioModificacion { get; set; }
+        public DateTime? asor_FechaModificacion { get; set; }
+
+    }
+}

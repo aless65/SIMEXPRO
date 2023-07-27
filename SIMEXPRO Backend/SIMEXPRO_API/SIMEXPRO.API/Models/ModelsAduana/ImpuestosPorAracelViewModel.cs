1ddb50f052e9ef7fc20e65380441f03d6dfc1926
itsInfinity03 <139721902+javintiktok@users.noreply.github.com>
2023-07-27 10:11:38 -0600

    Controllers Aduanas

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Models/ModelsAduana/ImpuestosPorAracelViewModel.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Models/ModelsAduana/ImpuestosPorAracelViewModel.cs
@@ -7,5 +7,13 @@ namespace SIMEXPRO.API.Models.ModelsAduana
 {
     public class ImpuestosPorAracelViewModel
     {
+        public int imar_Id { get; set; }
+        public int impu_Id { get; set; }
+        public int aran_Id { get; set; }
+        public int usua_UsuarioCreacion { get; set; }
+        public DateTime imar_FechaCreacion { get; set; }
+        public int? usua_UsuarioModificacion { get; set; }
+        public DateTime? imar_FechaModificacion { get; set; }
+        public bool? imar_Estado { get; set; }
     }
 }
--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Models/ModelsAduana/LugaresEmbarqueViewModel.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Models/ModelsAduana/LugaresEmbarqueViewModel.cs
@@ -7,5 +7,15 @@ namespace SIMEXPRO.API.Models.ModelsAduana
 {
     public class LugaresEmbarqueViewModel
     {
+        public int emba_Id { get; set; }
+        public string emba_Codigo { get; set; }
+        public string emba_Descripcion { get; set; }
+        public int usua_UsuarioCreacion { get; set; }
+        public DateTime emba_FechaCreacion { get; set; }
+        public int? usua_UsuarioModificacion { get; set; }
+        public DateTime? emba_FechaModificacion { get; set; }
+        public int? usua_UsuarioEliminacion { get; set; }
+        public DateTime? emba_FechaEliminacion { get; set; }
+        public bool? emba_Estado { get; set; }
     }
 }

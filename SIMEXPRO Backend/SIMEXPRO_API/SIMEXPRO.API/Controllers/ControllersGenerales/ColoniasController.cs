1072cd001dc77bdc7b5d6ee4bca754b480390541
juans <98434208+aless65@users.noreply.github.com>
2023-07-31 10:06:12 -0600

    trabajo en token y cambios en declaraciones valor

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Controllers/ControllersGenerales/ColoniasController.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Controllers/ControllersGenerales/ColoniasController.cs
@@ -23,11 +23,12 @@ namespace SIMEXPRO.API.Controllers.ControllersGenerales
             _generalesServices = generalesService;
             _mapper = mapper;
         }
+
         [HttpGet("Listado")]
         public IActionResult Index()
         {
             var listado = _generalesServices.ListarColonias();
-            var listadoMapeado = _mapper.Map<IEnumerable<ColoniasViewModel>>(listado);
+            var listadoMapeado = _mapper.Map<IEnumerable<ColoniasViewModel>>(listado.Data);
             return Ok(listadoMapeado);
         }
 

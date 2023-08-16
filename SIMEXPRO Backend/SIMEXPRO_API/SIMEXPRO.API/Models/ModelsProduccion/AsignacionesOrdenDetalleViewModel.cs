9944d67065bd811e4302bb26af3f612849f98ec7
aless <alessia88@hotmail.com>
2023-08-16 10:55:36 -0600

    viendo

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Models/ModelsProduccion/AsignacionesOrdenDetalleViewModel.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Models/ModelsProduccion/AsignacionesOrdenDetalleViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SIMEXPRO.API.Models.ModelsProduccion
 {

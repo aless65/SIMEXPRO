dd2362ccaa60ebf72b44643208e8b1b4ee53b3e6
SRMAZ2023 <125476485+SRMAZ2023@users.noreply.github.com>
2023-08-17 13:55:39 -0600

    DocumentosCompra

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Controllers/ControllersProduccion/DocumentosOrdenCompraDetallesController.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.API/Controllers/ControllersProduccion/DocumentosOrdenCompraDetallesController.cs
@@ -1,61 +1,61 @@
-﻿//using AutoMapper;
-//using Microsoft.AspNetCore.Mvc;
-//using SIMEXPRO.BussinessLogic.Services.AccesoServices;
-//using SIMEXPRO.BussinessLogic.Services.ProduccionServices;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SIMEXPRO.API.Models.ModelsProduccion;
+ using SIMEXPRO.BussinessLogic.Services.ProduccionServices;
+using SIMEXPRO.Entities.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace SIMEXPRO.API.Controllers.ControllersProduccion
-//{
+namespace SIMEXPRO.API.Controllers.ControllersProduccion
+{
 
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class DocumentosOrdenCompraDetallesController : Controller
-//    {
-//        private readonly ProduccionServices _produccionServices;
-//        private readonly IMapper _mapper;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentosOrdenCompraDetallesController : Controller
+    {
+        private readonly ProduccionServices _produccionServices;
+        private readonly IMapper _mapper;
 
-//        public DocumentosOrdenCompraDetallesController(ProduccionServices produccionServices, IMapper mapper)
-//        {
-//            _produccionServices = produccionServices;
-//            _mapper = mapper;
-//        }
+        public DocumentosOrdenCompraDetallesController(ProduccionServices produccionServices, IMapper mapper)
+        {
+            _produccionServices = produccionServices;
+            _mapper = mapper;
+        }
 
 
-//        [HttpGet("Listar")]
-//        public IActionResult Index()
-//        {
-//            var listado = _produccionServices.ListarDocumentosOrdenCompraDetalles();
+        [HttpGet("Listar")]
+        public IActionResult Index()
+        {
+            var listado = _produccionServices.ListarDocumentosOrdenCompraDetalles();
 
-//            listado.Data = _mapper.Map<IEnumerable<DocumentosOrdenCompraDetallesViewModel>>(listado.Data);
+            listado.Data = _mapper.Map<IEnumerable<DocumentosOrdenCompraDetallesViewModel>>(listado.Data);
 
-//            return Ok(listado);
-//        }
+            return Ok(listado);
+        }
 
-//        [HttpPost("Insertar")]
-//        public IActionResult Insertar(DocumentosOrdenCompraDetallesViewModel tbDocumentosOrdenCompraDetalles)
-//        {
-//            var mapped = _mapper.Map<tbDocumentosOrdenCompraDetalles>(tbDocumentosOrdenCompraDetalles);
-//            var datos = _produccionServices.InsertarDocumentosOrdenCompraDetalles(mapped);
-//            return Ok(datos);
-//        }
+        [HttpPost("Insertar")]
+        public IActionResult Insertar(DocumentosOrdenCompraDetallesViewModel tbDocumentosOrdenCompraDetalles)
+        {
+            var mapped = _mapper.Map<tbDocumentosOrdenCompraDetalles>(tbDocumentosOrdenCompraDetalles);
+            var datos = _produccionServices.InsertarDocumentosOrdenCompraDetalles(mapped);
+            return Ok(datos);
+        }
 
-//        [HttpPost("Editar")]
-//        public IActionResult Editar(DocumentosOrdenCompraDetallesViewModel tbDocumentosOrdenCompraDetalles)
-//        {
-//            var mapped = _mapper.Map<tbDocumentosOrdenCompraDetalles>(tbDocumentosOrdenCompraDetalles);
-//            var datos = _produccionServices.ActualizarDocumentosOrdenCompraDetalles(mapped);
-//            return Ok(datos);
-//        }
+        [HttpPost("Editar")]
+        public IActionResult Editar(DocumentosOrdenCompraDetallesViewModel tbDocumentosOrdenCompraDetalles)
+        {
+            var mapped = _mapper.Map<tbDocumentosOrdenCompraDetalles>(tbDocumentosOrdenCompraDetalles);
+            var datos = _produccionServices.ActualizarDocumentosOrdenCompraDetalles(mapped);
+            return Ok(datos);
+        }
 
-//        [HttpPost("Eliminar")]
-//        public IActionResult Eliminar(DocumentosOrdenCompraDetallesViewModel tbDocumentosOrdenCompraDetalles)
-//        {
-//            var mapped = _mapper.Map<tbDocumentosOrdenCompraDetalles>(tbDocumentosOrdenCompraDetalles);
-//            var datos = _produccionServices.EliminarDocumentosOrdenCompraDetalles(mapped);
-//            return Ok(datos);
-//        }
-//    }
-//}
+        [HttpPost("Eliminar")]
+        public IActionResult Eliminar(DocumentosOrdenCompraDetallesViewModel tbDocumentosOrdenCompraDetalles)
+        {
+            var mapped = _mapper.Map<tbDocumentosOrdenCompraDetalles>(tbDocumentosOrdenCompraDetalles);
+            var datos = _produccionServices.EliminarDocumentosOrdenCompraDetalles(mapped);
+            return Ok(datos);
+        }
+    }
+}

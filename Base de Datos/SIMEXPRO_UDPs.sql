7fced3ba75dadede80807722084f29fcc9e8348a
Alexander <125475633+ALeaxanderaz04@users.noreply.github.com>
2023-08-21 07:45:57 -0600

    Procedimiento Pedidos Produccion Detalle

--- Base de Datos/SIMEXPRO_UDPs.sql
+++ Base de Datos/SIMEXPRO_UDPs.sql
@@ -13520,6 +13520,74 @@ END
 GO
 
 
+CREATE OR ALTER PROC Prod.UDP_tbPedidosProduccionDetalle_Insertar 
+(@ppro_Id INT,
+ @lote_Id INT,
+ @ppde_Cantidad INT,
+ @usua_UsuarioCreacion INT,
+ @ppde_FechaCreacion DATETIME)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO [Prod].[tbPedidosProduccionDetalles] ([ppro_Id], [lote_Id], [ppde_Cantidad], [usua_UsuarioCreacion], [ppde_FechaCreacion])
+		VALUES (@ppro_Id,@lote_Id,@lote_Id,@usua_UsuarioCreacion,@ppde_FechaCreacion);
+
+		SELECT 1
+	END TRY
+	BEGIN CATCH
+			SELECT 'Error Message: ' + ERROR_MESSAGE()
+	END CATCH
+END
+
+
+
+GO
+CREATE OR ALTER PROC Prod.UDP_tbPedidosProduccionDetalle_Editar 
+(@ppde_Id					INT,
+ @ppro_Id					INT,
+ @lote_Id					INT,
+ @ppde_Cantidad				INT,
+ @usua_UsuarioModificacion	INT,
+ @ppde_FechaModificacion DATETIME)
+AS
+BEGIN
+	BEGIN TRY
+
+		UPDATE [Prod].[tbPedidosProduccionDetalles]
+		SET [ppro_Id] = @ppro_Id, 
+			[lote_Id] = @lote_Id, 
+			[ppde_Cantidad] = @ppde_Cantidad , 
+			[usua_UsuarioModificacion] = @usua_UsuarioModificacion, 
+			[ppde_FechaCreacion] = @ppde_FechaModificacion
+		WHERE ppde_Id = @ppde_Id
+
+		SELECT 1
+	END TRY
+	BEGIN CATCH
+			SELECT 'Error Message: ' + ERROR_MESSAGE()
+	END CATCH
+END
+
+
+
+GO
+CREATE OR ALTER PROC Prod.UDP_tbPedidosProduccionDetalle_Eliminar
+(@ppde_Id					INT)
+AS
+BEGIN
+	BEGIN TRY
+
+		UPDATE [Prod].[tbPedidosProduccionDetalles]
+		SET [ppde_Estado] = 0
+		WHERE ppde_Id = @ppde_Id
+
+		SELECT 1
+	END TRY
+	BEGIN CATCH
+			SELECT 'Error Message: ' + ERROR_MESSAGE()
+	END CATCH
+END
+
 --************************************************************************   Tabla Modulos fin   ***********************************************************************************************
 GO
 --************************************************************************   Tabla Maquinas inicio   ***********************************************************************************************

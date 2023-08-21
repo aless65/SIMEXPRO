d0bc9861b5e5b8a7f0dbaac3974c8f2d9b23a2aa
SRMAZ2023 <125476485+SRMAZ2023@users.noreply.github.com>
2023-08-21 13:39:40 -0600

    campos mapeados

--- Base de Datos/SIMEXPRO_UDPs.sql
+++ Base de Datos/SIMEXPRO_UDPs.sql
@@ -12872,6 +12872,11 @@ BEGIN
 			rdet_TotalDia, 
 			rdet_TotalDanado, 
 			OrdenCompra.orco_Id,
+			colores.colr_Nombre,
+			case ordencompradetalle.code_Sexo 
+			when 'M' then 'Masculino'
+			when 'F' then 'Femenino'
+			else ordencompradetalle.code_Sexo end as Sexo,
 			clientes.[clie_Nombre_Contacto],
 			clientes.[clie_RTN],
  			ReporteModuloDia.code_Id, 
@@ -12887,6 +12892,7 @@ BEGIN
 			INNER JOIN Prod.tbEstilos			estilos					ON ordencompradetalle.esti_Id = estilos.esti_Id
 			INNER JOIN Prod.tbOrdenCompra		OrdenCompra				ON	ordencompradetalle.orco_Id = OrdenCompra.orco_Id
 			INNER JOIN Prod.tbClientes			clientes				ON  OrdenCompra.orco_IdCliente = clientes.clie_Id
+			INNER JOIN Prod.tbColores			colores					ON	ordencompradetalle.code_Id	= colores.colr_Id
 			WHERE ReporteModuloDia.remo_Id = @remo_Id
 
 	

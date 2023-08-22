ceda31401eb33daf8aca24c7d205b3a9ba44460c
LA MAMI <139721902+javintiktok@users.noreply.github.com>
2023-08-22 15:10:17 -0600

    yo opino que bajen la api

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Prod/Orde_Ensa_Acab_EtiqRepository.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Prod/Orde_Ensa_Acab_EtiqRepository.cs
@@ -34,6 +34,7 @@ namespace SIMEXPRO.DataAccess.Repositories.Prod
             parametros.Add("@ensa_FechaInicio", item.ensa_FechaInicio, DbType.DateTime, ParameterDirection.Input);
             parametros.Add("@ensa_FechaLimite", item.ensa_FechaLimite, DbType.DateTime, ParameterDirection.Input);
             parametros.Add("@ppro_Id", item.ppro_Id, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@modu_Id", item.modu_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@usua_UsuarioCreacion", item.usua_UsuarioCreacion, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@ensa_FechaCreacion", item.ensa_FechaCreacion, DbType.DateTime, ParameterDirection.Input);
 
@@ -61,6 +62,7 @@ namespace SIMEXPRO.DataAccess.Repositories.Prod
             parametros.Add("@ensa_FechaInicio", item.ensa_FechaInicio, DbType.DateTime, ParameterDirection.Input);
             parametros.Add("@ensa_FechaLimite", item.ensa_FechaLimite, DbType.DateTime, ParameterDirection.Input);
             parametros.Add("@ppro_Id", item.ppro_Id, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@modu_Id", item.modu_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@usua_UsuarioCreacion", item.usua_UsuarioCreacion, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@ensa_FechaCreacion", item.ensa_FechaCreacion, DbType.DateTime, ParameterDirection.Input);
 

5102393819995b1e754d8c579e7a0739068d1d49
Ceci <karlaalejandro01@gmail.com>
2023-08-21 21:17:33 -0600

    CAMBIOS EN API QUITANDO LOS CAMPOS QUE SE QUITARON EN LA BASE

--- SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Prod/PedidosOrdenDetallesRepository.cs
+++ SIMEXPRO Backend/SIMEXPRO_API/SIMEXPRO.DataAccess/Repositories/Prod/PedidosOrdenDetallesRepository.cs
@@ -32,7 +32,7 @@ namespace SIMEXPRO.DataAccess.Repositories.Prod
             parametros.Add("@mate_Id", item.mate_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@prod_Cantidad", item.prod_Cantidad, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@prod_Precio", item.prod_Precio, DbType.Decimal, ParameterDirection.Input);
-            parametros.Add("@prod_Peso", item.prod_Peso, DbType.Decimal, ParameterDirection.Input);
+          //  parametros.Add("@prod_Peso", item.prod_Peso, DbType.Decimal, ParameterDirection.Input);
             parametros.Add("@usua_UsuarioCreacion", item.usua_UsuarioCreacion, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@prod_FechaCreacion", item.prod_FechaCreacion, DbType.DateTime, ParameterDirection.Input);
 
@@ -58,7 +58,7 @@ namespace SIMEXPRO.DataAccess.Repositories.Prod
             parametros.Add("@mate_Id", item.mate_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@prod_Cantidad", item.prod_Cantidad, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@prod_Precio", item.prod_Precio, DbType.Decimal, ParameterDirection.Input);
-            parametros.Add("@prod_Peso", item.prod_Peso, DbType.Decimal, ParameterDirection.Input);
+          //  parametros.Add("@prod_Peso", item.prod_Peso, DbType.Decimal, ParameterDirection.Input);
             parametros.Add("@usua_UsuarioCreacion", item.usua_UsuarioCreacion, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@prod_FechaCreacion", item.prod_FechaCreacion, DbType.DateTime, ParameterDirection.Input);
 

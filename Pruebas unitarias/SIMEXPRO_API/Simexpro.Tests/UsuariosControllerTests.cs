150ff666e149972e21d6a524bf17bf4271c89157
aless65 <140451736+SIMEXPRO@users.noreply.github.com>
2023-10-15 01:20:28 -0600

    ¿eso es normal?

--- Pruebas unitarias/SIMEXPRO_API/Simexpro.Tests/UsuariosControllerTests.cs
+++ Pruebas unitarias/SIMEXPRO_API/Simexpro.Tests/UsuariosControllerTests.cs
@@ -20,30 +20,32 @@ namespace Simexpro.Tests
     public class UsuariosControllerTests
     {
         private readonly UsuariosController _usuariosController;
-        //private readonly AccesoServices _accesoServices;
-        private readonly Mock<AccesoServices> _accesoServices;
+        private readonly AccesoServices _accesoServices;
+        //private readonly Mock<AccesoServices> _accesoServices;
         private readonly IMapper _mapper;
 
         public UsuariosControllerTests()
         {
             // Dependencies
-            PantallasRepository pantallasRepository = A.Fake<PantallasRepository>();
-            RolesRepository rolesRepository = A.Fake<RolesRepository>();
-            RolesPorPantallaRepository rolesPorPantallaRepository = A.Fake<RolesPorPantallaRepository>();
-            UsuariosRepository usuariosRepository = A.Fake<UsuariosRepository>();
-            UsuariosHistorialRepository usuariosHistorialRepository = A.Fake<UsuariosHistorialRepository>();
+            //PantallasRepository pantallasRepository = A.Fake<PantallasRepository>();
+            //RolesRepository rolesRepository = A.Fake<RolesRepository>();
+            //RolesPorPantallaRepository rolesPorPantallaRepository = A.Fake<RolesPorPantallaRepository>();
+            //UsuariosRepository usuariosRepository = A.Fake<UsuariosRepository>();
+            //UsuariosHistorialRepository usuariosHistorialRepository = A.Fake<UsuariosHistorialRepository>();
 
-            _accesoServices = new Mock<AccesoServices>(
-                pantallasRepository,
-                rolesRepository,
-                rolesPorPantallaRepository,
-                usuariosRepository,
-                usuariosHistorialRepository
-            );
+            //_accesoServices = new Mock<AccesoServices>(
+            //    pantallasRepository,
+            //    rolesRepository,
+            //    rolesPorPantallaRepository,
+            //    usuariosRepository,
+            //    usuariosHistorialRepository
+            //);
+            //_accesoServices = accesoServices;
+            _accesoServices = A.Fake<AccesoServices>();
             _mapper = A.Fake<IMapper>();
 
             //SUT
-            _usuariosController = new UsuariosController(_accesoServices.Object, _mapper);
+            _usuariosController = new UsuariosController(_accesoServices, _mapper);
 
         }
 
@@ -52,14 +54,15 @@ namespace Simexpro.Tests
         {
             // Arrange - ¿Qué necesito traer?
             var usuariosServiceResult = A.Fake<ServiceResult>();
-            //A.CallTo(() => _accesoServices.ListarUsuarios(true)).Returns(usuariosServiceResult);
-            _accesoServices.Setup(x => x.ListarUsuarios(It.IsAny<bool?>())).Returns(usuariosServiceResult);
+            A.CallTo(() => _accesoServices.ListarUsuarios(It.IsAny<bool?>())).Returns(usuariosServiceResult);
+            //_accesoServices.Setup(x => x.ListarUsuarios(It.IsAny<bool?>())).Returns(usuariosServiceResult);
 
             // Act
             var result = _usuariosController.Index(true);
 
             // Assert - Acciones para chequear objetos
             result.Should().BeOfType<IActionResult>();
+            //Assert.IsType<ServiceResult>(result);
         }
     }
 }

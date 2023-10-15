e7f909787f56e0138234d90c0365c2c43409441e
❀ jumAngie.FM ❀ <125337384+jumAngie@users.noreply.github.com>
2023-10-15 17:04:21 -0600

    no soca esa vaina

--- Pruebas unitarias/SIMEXPRO_API/OrdenDeCompraInsertar/OrdenCompraServicesTest.cs
+++ Pruebas unitarias/SIMEXPRO_API/OrdenDeCompraInsertar/OrdenCompraServicesTest.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SIMEXPRO.API.Controllers.ControllersProduccion;
+using SIMEXPRO.BussinessLogic;
+using SIMEXPRO.BussinessLogic.Services.ProduccionServices;
+using SIMEXPRO.Entities.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace OrdenDeCompraInsertar
 {
-    class OrdenCompraServicesTest
+    public class OrdenCompraServicesTest
     {
+        
     }
 }

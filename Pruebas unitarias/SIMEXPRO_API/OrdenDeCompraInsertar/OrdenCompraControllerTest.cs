4544ba1268c0a2b9c9bfb9c571216d110f4478ee
❀ jumAngie.FM ❀ <125337384+jumAngie@users.noreply.github.com>
2023-10-15 18:47:09 -0600

    tira errorcitos tamos chamebando en ellos

--- Pruebas unitarias/SIMEXPRO_API/OrdenDeCompraInsertar/OrdenCompraControllerTest.cs
+++ Pruebas unitarias/SIMEXPRO_API/OrdenDeCompraInsertar/OrdenCompraControllerTest.cs
@@ -3,6 +3,8 @@ using FakeItEasy;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using NSubstitute;
+using NUnit.Framework;
 using SIMEXPRO.API.Controllers.ControllersProduccion;
 using SIMEXPRO.BussinessLogic;
 using SIMEXPRO.BussinessLogic.Services.ProduccionServices;
@@ -10,6 +12,7 @@ using SIMEXPRO.Entities.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -18,6 +21,39 @@ namespace OrdenDeCompraInsertar
 {
     public class OrdenCompraControllerTest
     {
-        
+        private OrdenCompraServicesTest ordenCompraServices;
+        private OrdenCompraRequest      ordenCompraRequest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ordenCompraServices = Substitute.For<OrdenCompraServicesTest>();
+            ordenCompraRequest = new OrdenCompraRequest()
+            {
+                Name = "Cristian",
+                Age = 30
+                // acá van las vainas q se rellenan en el swagger
+            };
+        }
+
+        [TestCase(HttpStatusCode.OK)]
+        [TestCase(HttpStatusCode.InternalServerError)]
+        public async Task Insertar_Orden_Compra(HttpStatusCode code)
+        {
+            //Arrange
+            GenericResponse response = new GenericResponse()
+            {
+                HttpCode = code
+            };
+
+            //Act
+            ordenCompraServices.Insertar(OrdenCompraRequest).ReturnsForAnyArgs(response);
+            OrdenCompraController controller = new OrdenCompraController(ordenCompraServices);
+            ObjectResult responseController = (ObjectResult)await controller.Insertar(OrdenCompraRequest);
+
+            //Assert
+            Assert.AreEqual((int)code, responseController.StatusCode.Value);
+        }
     }
+
 }

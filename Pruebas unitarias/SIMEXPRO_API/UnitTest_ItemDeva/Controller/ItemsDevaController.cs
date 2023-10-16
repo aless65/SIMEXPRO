6137ecf8f5cdec5024798a975f2bc7f4e7a0432f
IanHernandez87 <ianh8902@gmail.com>
2023-10-16 12:37:31 -0600

    cambios

--- Pruebas unitarias/SIMEXPRO_API/UnitTest_ItemDeva/Controller/ItemsDevaController.cs
+++ Pruebas unitarias/SIMEXPRO_API/UnitTest_ItemDeva/Controller/ItemsDevaController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using SIMEXPRO.API.Controllers.ControllersAduanas;
 using SIMEXPRO.DataAccess;
 using SIMEXPRO.DataAccess.Repositories.Adua;
@@ -11,40 +12,34 @@ namespace UnitTest_ItemDeva
     {
         ItemsRepository _itemsRepository = new ItemsRepository();
 
-        private readonly ItemsController _itemsController;
 
-        public ItemsDevaController(ItemsController itemsController)
-        {
-            _itemsController = itemsController;
-        }
+        //[Fact]
+        //public void TestMasiso()
+        //{
+        //    tbItems item = new tbItems()
+        //    {
+        //        fact_Id = 24,
+        //        item_Cantidad = 1500,
+        //        unme_Id = 1,
+        //        item_IdentificacionComercialMercancias = "Tecnologia",
+        //        item_CaracteristicasMercancias = "Fragil",
+        //        item_Marca = "Estrukture",
+        //        item_Modelo = "Max RYT",
+        //        merc_Id = 2,
+        //        pais_IdOrigenMercancia = 3,
+        //        aran_Id = 22401,
+        //        item_ValorUnitario = 2500,
+        //        item_ValorTransaccion = 15000,
+        //        usua_UsuarioCreacion = 1,
+        //        item_FechaCreacion = new DateTime(2023, 10, 16, 01, 15, 00),
+        //        item_EsNuevo = false,
+        //        item_EsHibrido = false,
+        //        item_LitrosTotales = 0,
+        //        item_CigarrosTotales = 0,
+        //    };
 
-        [Fact]
-        public void TestMasiso()
-        {
-            tbItems item = new tbItems()
-            {
-                fact_Id = 24,
-                item_Cantidad = 1500,
-                unme_Id = 1,
-                item_IdentificacionComercialMercancias = "Tecnologia",
-                item_CaracteristicasMercancias = "Fragil",
-                item_Marca = "Estrukture",
-                item_Modelo = "Max RYT",
-                merc_Id = 2,
-                pais_IdOrigenMercancia = 3,
-                aran_Id = 22401,
-                item_ValorUnitario = 2500,
-                item_ValorTransaccion = 15000,
-                usua_UsuarioCreacion = 1,
-                item_FechaCreacion = new DateTime(2023, 10, 16, 01, 15, 00),
-                item_EsNuevo = false,
-                item_EsHibrido = false,
-                item_LitrosTotales = 0,
-                item_CigarrosTotales = 0,
-            };
-
-            IActionResult result = _ducaController.EditarPart1(datos);
-        }
+        //    IActionResult result = _ducaController.EditarPart1(datos);
+        //}
 
 
 
@@ -66,7 +61,7 @@ namespace UnitTest_ItemDeva
                 item_ValorUnitario = 2500,
                 item_ValorTransaccion = 15000,
                 usua_UsuarioCreacion = 1,
-                item_FechaCreacion = new DateTime(2023, 10, 16, 01, 15, 00),
+                item_FechaCreacion = new DateTime(2023, 10, 10, 01, 15, 00),
                 item_EsNuevo = false,
                 item_EsHibrido = false,
                 item_LitrosTotales = 0,
